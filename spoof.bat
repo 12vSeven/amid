3ec7ed12dfb617c4dd75bd16bb49456f97a43d60
12vSeven <129784582+12vSeven@users.noreply.github.com>
2025-03-06 23:04:29 +0100

    Add files via upload

---
+++ spoof.bat
@@ -0,0 +1,21 @@
+@echo off
+C:\Windows\debug\AMIDEWINx64.EXE /IVN "AMI" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /SP "ProBook 450 G6" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /SV "Surface Laptop 3" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /SS %RANDOM%%RANDOM%%RANDOM% >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /SU %RANDOM%%RANDOM%%RANDOM% >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /SK %RANDOM%%RANDOM%%RANDOM% >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /BM "AsRock" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /BP "P-8560M-C-2025" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /BS %RANDOM%%RANDOM%%RANDOM% >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /BT "Default String" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /BLC "Default String" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /CM "Default String" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /CV "Default String" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /CS %RANDOM%%RANDOM%%RANDOM% >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /CA "Default String" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /CSK "ESANZ-97585" >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /PSN %RANDOM%%RANDOM%%RANDOM% >> C:\Windows\debug\amidewin_log.txt 2>&1
+C:\Windows\debug\AMIDEWINx64.EXE /PAT %RANDOM%%RANDOM%%RANDOM% >> C:\Windows\debug\amidewin_log.txt 2>&1
+
+exit
